6c2f7d20bcc364b3100d6698ac6a74941120a9b5
Artem <artess.ponomarev@mail.ru>
2023-02-26 17:36:06 +0300

    created header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import cl from './Header.module.scss';
+
+const Header: React.FC = () => {
+    return (
+        <div className={cl.header}>
+            <h2>Git search</h2>
+        </div>
+    );
+};
+
+export default Header;
